17bf412e35aa10cc11f905cadd4a87726601a895
Phan Huy Hoang <hoangph271@gmail.com>
2022-10-10 16:37:41 +0700

    [Add] open_api_features

--- src/challenge.rs
+++ src/challenge.rs
@@ -12,3 +12,21 @@ pub struct Challenge {
     pub end_at: DateTime<Utc>,
     pub finished: bool,
 }
+
+#[cfg(feature = "okapi")]
+mod open_api_features {
+    use super::*;
+    use okapi::openapi3::Responses;
+    use rocket_okapi::{gen::OpenApiGenerator, response::OpenApiResponderInner};
+    use serde::Serialize;
+
+    impl<T: Serialize> OpenApiResponderInner for Challenge {
+        fn responses(
+            _: &mut rocket_okapi::gen::OpenApiGenerator,
+        ) -> rocket_okapi::Result<okapi::openapi3::Responses> {
+            Ok(Responses {
+                ..Default::default()
+            })
+        }
+    }
+}
